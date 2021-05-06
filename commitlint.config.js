643d574fbffc0f455fd0b45753d14e37ed7f784e
guanxin <373103730@qq.com>
2021-05-06 14:48:57 +0800

    feat(y): y
    y
    
    BREAKING CHANGE: y
    
    y

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
-    },
-};
+module.exports = { extends: ['@commitlint/config-conventional'] };
